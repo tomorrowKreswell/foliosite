4191ff765f2283d8e6b95191facd8df11daafc53
yancyl <yanerbuer@163.com>
2024-05-28 18:34:59 +0800

    Updates

---
+++ precache-manifest.e20c7e06e2312f48f32050aa58cf4ac9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4471c98846d45ec5e74f",
+    "url": "/foliosite/static/css/main.4a4b064b.chunk.css"
+  },
+  {
+    "revision": "4471c98846d45ec5e74f",
+    "url": "/foliosite/static/js/main.4471c988.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/foliosite/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "3f907d51886e8545673a",
+    "url": "/foliosite/static/js/runtime~main.3f907d51.js"
+  },
+  {
+    "revision": "39ecc1ff24cd0e21c07555941625718b",
+    "url": "/foliosite/index.html"
+  }
+];
